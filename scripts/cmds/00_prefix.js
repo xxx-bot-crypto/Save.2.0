a5623347fb70377c7a302ffa55b76d5a4dfae583
goatbotnx <onlygoatbot.nx@gmail.com>
2025-11-05 07:47:18 +0600

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -29,7 +29,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "🌐 System prefix: %1\n🛸 Your box chat prefix: %2"
+			myPrefix: "🌐 System prefix: %1\n🛸 Your box chat prefix: %2\n🧑‍💻Owner inbox https://m.me/nx210.2.0.is.back"
 		}
 	},
 
